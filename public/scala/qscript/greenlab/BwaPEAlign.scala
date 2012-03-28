b6fb4dbf6f71bf1789109fb27f39bbe862d5efb3
John St. John <johnthesaintjohn@gmail.com>
2012-03-27 17:47:12 -0700

    Adding two new Sting Queue pipelines to work on

---
+++ public/scala/qscript/greenlab/BwaPEAlign.scala
@@ -0,0 +1,13 @@
+package public.scala.qscript.greenlab
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jstjohn
+ * Date: 3/27/12
+ * Time: 4:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class BwaPEAlign {
+
+}
